5460bc2ca621d04cad5da9459499f57d6dff5a2d
PuskarMal <puskarmal07@gmail.com>
2026-01-12 00:14:19 +0530

    new page urls added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,35 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+
 import Navbar from '../components/Navbar/Navbar'
 import Footer from '../components/Footer/Footer'
 import BreedAnalytics from '../pages/BreedAnalytics'
-import { Route,Routes } from 'react-router-dom'
+import RegisterCattle from '../pages/RegisterCattle'
+import CattleProfile from '../pages/CattleProfile'
+import VoiceBot from '../pages/VoiceBot'
+import VoicebotLogo from '../components/VoiceBotLogo/VoiceBotLogo'
+import Marketplace from '../pages/Marketplace'
+
 import Home from '../pages/Home'
-import "./i18n"; 
+
+import "./i18n";
+
 const App = () => {
   return (
     <div>
-      
-      <Navbar/>
+
+      <Navbar />
       <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/analytics" element={<BreedAnalytics />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/analytics" element={<BreedAnalytics />} />
+        <Route path="/marketplace" element={<Marketplace />} />
+        <Route path="/cattle-profile" element={<CattleProfile />} />
+        <Route path="/register-cattle" element={<RegisterCattle />} />
+        <Route path="/voicebot" element={<VoiceBot />} />
       </Routes>
-      <Footer/>
-      
+      <VoicebotLogo/>
+      <Footer />
+
     </div>
   )
 }
