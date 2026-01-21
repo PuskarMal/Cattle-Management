d65acc6c028ba9fb040488693e64c2adf1ba5d27
PuskarMal <puskarmal07@gmail.com>
2026-01-22 00:33:59 +0530

    new apis added

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-
+const {initGridFS} = require("./config/gridfs.js");
 require("dotenv").config();
 require("./db.js");
 app.set("view engine", "ejs");
@@ -9,7 +9,7 @@ app.set("view engine", "ejs");
 const allowedOrigins = [
   "http://localhost:5173"
 ];
-
+initGridFS()
 const corsOptions = {
   origin: (origin, callback) => {
     if (!origin || allowedOrigins.includes(origin)) {
@@ -26,8 +26,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 const predictRoute = require("./routes/predict.js");
+const registerCattleRoute = require("./routes/registercattle.js");
+const report = require("./routes/report.js")
+const biometric = require("./routes/biometric.js")
+
+
+app.use("/", registerCattleRoute);
 
 app.use("/predict", predictRoute);
+app.use("/",report);
+app.use("/",biometric)
 
 app.listen(process.env.PORT || 3000, () => {
     console.log(`Server is running on port http://localhost:${process.env.PORT || 3000}`);
