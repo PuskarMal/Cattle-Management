d4e350b08877a9f9085aa2268e9558a2fa9179fb
PuskarMal <puskarmal07@gmail.com>
2026-02-01 12:57:43 +0530

    user model new fields added: user id, cattle, mongoose model name corrected... clean connection with database

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
+const cattle = require('./cattle');
 
 const userSchema = new mongoose.Schema({
+  user_id: {
+    type: String,
+    required: true
+  },
   full_name: { type: String, required: true }, // e.g., "Ramesh Kumar"
   role: { type: String, enum: ['farmer', 'vet', 'admin'], default: 'farmer' },
   phone_number: { type: String, required: true }, // e.g., "+91XXXXXXXX"
@@ -11,20 +16,24 @@ const userSchema = new mongoose.Schema({
     district: { type: String, required: true }, // e.g., "Junagadh"
     village: { type: String, required: true } // e.g., "Manbazar"
   },
+  cattle: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "cattle_masters"
+  }],
   password: { type: String, required: true }, // Hashed
   email: { type: String, required: true, unique: true } // For login
 }, { timestamps: true }); // Auto-add createdAt/updatedAt
 
 // Hash password before saving
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
+userSchema.pre('save', async function () {
+  if (!this.isModified('password')) return;
   this.password = await bcrypt.hash(this.password, 10);
-  next();
 });
+
 
 // Method to compare passwords
 userSchema.methods.comparePassword = async function (password) {
   return bcrypt.compare(password, this.password);
 };
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('users', userSchema);
