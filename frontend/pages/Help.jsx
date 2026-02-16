b6da7a48d635a6217b729d3a8c911cae69e6d671
PuskarMal <puskarmal07@gmail.com>
2026-02-16 22:07:18 +0530

    help page

---
+++ frontend/pages/Help.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import HelpPage from '../components/Dashboard/Help'
+const Help = () => {
+  return (
+    <div className="p-6 min-h-screen max-w-5xl mx-auto">
+        <h2 className="text-2xl font-semibold mb-6 border-b pb-2">
+        Help & Support
+      </h2>
+      <HelpPage />
+    </div>
+  )
+}
+
+export default Help
