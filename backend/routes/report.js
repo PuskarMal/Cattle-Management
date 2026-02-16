85e1afb73c06850259a0724d42b67e79842d5d39
PuskarMal <puskarmal07@gmail.com>
2026-02-16 21:58:54 +0530

    report generation and visualization update

--- backend/routes/report.js
+++ backend/routes/report.js
@@ -1,79 +1,85 @@
+const mongoose = require("mongoose"); 
 const PDFDocument = require("pdfkit");
 const Cattle = require("../models/cattle.js");
-
+const {getGridFSBucket} = require("../config/gridfs");
 const QRCode = require("qrcode");
 const { ChartJSNodeCanvas } = require("chartjs-node-canvas");
 const router = require("express").Router()
+const Report = require("../models/report.js");
+const Activity = require("../models/activity.js");
 
 router.get("/download-report/:unique_id", async (req, res) => {
   try {
-    const cattle = await Cattle.findOne({
-      unique_id: req.params.unique_id
-    });
+    const cattle = await Cattle.findOne({ unique_id: req.params.unique_id });
+    if (!cattle) return res.status(404).json({ error: "Cattle not found" });
 
-    if (!cattle) {
-      return res.status(404).json({ error: "Cattle not found" });
-    }
+    const report = await Report.findOne({ cattle_id: cattle._id });
+    if (!report) return res.status(404).json({ error: "Report not found" });
 
-    const doc = new PDFDocument({ margin: 50 });
+    const bucket = getGridFSBucket();
 
     res.setHeader("Content-Type", "application/pdf");
     res.setHeader(
       "Content-Disposition",
-      `attachment; filename=${cattle.cattle_id}_report.pdf`
+      `attachment; filename=${cattle.unique_id}_report.pdf`
     );
 
-    doc.pipe(res);
+    bucket.openDownloadStream(report.pdf_file_id).pipe(res);
 
-    doc.image("assets/logo.jpeg", 50, 40, { width: 80 });
-
-    doc.fontSize(18).text("Cattle Health & Production Report", { align: "center" });
-    doc.moveDown();
-    const qrData = `http://localhost:5173/register-cattle`;
-const qrImage = await QRCode.toDataURL(qrData);
-
-doc.image(qrImage, 450, 40, { width: 100 });
-    doc.fontSize(12).text(`Pashu AADHAR No: ${cattle.unique_id}`);
-    doc.fontSize(12).text(`Cattle ID: ${cattle.animal_tag_id}`);
-    doc.text(`Species: ${cattle.species}`);
-    doc.text(`Breed: ${cattle.breed_name}`);
-    doc.text(`Age (months): ${cattle.age_in_months}`);
-    doc.text(`Gender: ${cattle.gender}`);
-    doc.text(`State: ${cattle.state}`);
-    doc.text(`District: ${cattle.district}`);
-
-    doc.moveDown();
-    doc.text("Milk Production");
-    doc.text(`• Average Yield (LPD): ${cattle.milk_production?.average_yield_lpd || "N/A"}`);
-    doc.text(`• Fat %: ${cattle.milk_production?.fat_percentage || "N/A"}`);
-
-    doc.moveDown();
-    doc.text("Health Information");
-    doc.text(`• Last Vaccination: ${cattle.health_status?.last_vaccination_date || "N/A"}`);
-    doc.text(`• Current Condition: ${cattle.health_status?.current_condition || "Normal"}`);
-    const chartCanvas = new ChartJSNodeCanvas({ width: 500, height: 300 });
-
-const chartImage = await chartCanvas.renderToBuffer({
-  type: "line",
-  data: {
-    labels: ["Jan", "Feb", "Mar", "Apr"],
-    datasets: [{
-      label: "Milk Yield (LPD)",
-      data: [10, 12, 11, 13],
-      borderColor: "green"
-    }]
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Download failed" });
   }
 });
 
-doc.addPage();
-doc.text("Milk Production Trend");
-doc.image(chartImage, 50, 100);
-    doc.image("assets/sign.jpg", 400, 650, { width: 120 });
-doc.text("Authorized Veterinary Officer", 400, 700);
-    doc.end();
+router.get("/reports", async (req, res) => {
+  const reports = await Report.find()
+    .populate("cattle_id", "unique_id breed_name")
+    .populate("owner_id", "full_name user_id")
+    .sort({ createdAt: -1 });
+
+  res.json(reports);
+});
+
+router.get("/report/pdf/:id", async (req, res) => {
+
+  const bucket = getGridFSBucket();
+
+  const fileId = new mongoose.Types.ObjectId(req.params.id);
+
+  res.set("Content-Type", "application/pdf");
+
+  bucket
+    .openDownloadStream(fileId)
+    .pipe(res);
+});
+
+router.get("/activity", async (req, res) => {
+  try {
+    const activities = await Activity.find()
+      .populate("performedBy", "full_name user_id")
+      .populate("entityId", "unique_id")
+      .sort({ createdAt: -1 });
+
+    res.json(activities);
   } catch (err) {
     console.error(err);
-    res.status(500).json({ error: "Failed to generate report" });
+    res.status(500).json({ error: "Failed to fetch activities" });
+  }
+});
+
+router.get("/recent-activity", async (req, res) => {
+  try {
+    const activities = await Activity.find()
+      .populate("performedBy", "full_name user_id")
+      .populate("entityId", "unique_id")
+      .sort({ createdAt: -1 })
+      .limit(3);
+
+    res.json(activities);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Failed to fetch activities" });
   }
 });
 
