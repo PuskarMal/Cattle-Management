fe1c21b32c9c0bd7004f81feeae4841e92263205
PuskarMal <puskarmal07@gmail.com>
2026-02-16 21:59:56 +0530

    family tree to include children

--- backend/utils/FamilyTree.js
+++ backend/utils/FamilyTree.js
@@ -13,6 +13,7 @@ async function buildFamilyTree(id, generations = 3, visited = new Set()) {
   const animal = await Cattle.findOne({unique_id: id})
     .populate("sire_id", "unique_id name breed_name gender")
     .populate("dam_id", "unique_id name breed_name gender")
+    .populate("children", "image_id unique_id name breed_name gender")
     .lean();
 
   if (!animal) return null;
@@ -21,6 +22,7 @@ async function buildFamilyTree(id, generations = 3, visited = new Set()) {
     ...animal,
     sire: await buildFamilyTree(animal.sire_id?.unique_id, generations - 1, visited),
     dam: await buildFamilyTree(animal.dam_id?.unique_id, generations - 1, visited)
+
   };
 }
 
