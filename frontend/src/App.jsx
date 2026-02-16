82741fbcbefa2dd650c4697513d0ccb131f92cec
PuskarMal <puskarmal07@gmail.com>
2026-02-16 22:12:44 +0530

    new page api added and rba added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import { Route, Routes } from 'react-router-dom'
 
 import Navbar from '../components/Navbar/Navbar'
@@ -16,8 +16,27 @@ import Signup from '../pages/SignUp'
 import Login from '../pages/Login'
 import Disease from '../pages/Disease'
 import "./i18n";
+import Reports from '../pages/Reports'
+import MyCattle from '../pages/MyCattle'
+import Activity from '../pages/Activity'
+import Help from '../pages/Help'
+import AdminMarketplace from '../pages/AdminMarketPlace'
+import { useDispatch, useSelector } from 'react-redux'
+import { authActions } from './store/auth'
 
 const App = () => {
+  const dispatch = useDispatch();
+  const role = useSelector((state) => state.auth.role);
+  useEffect(() => {
+    if (
+        localStorage.getItem("id") &&
+        localStorage.getItem("token") &&
+        localStorage.getItem("role")
+    ){
+      dispatch(authActions.login());
+      dispatch(authActions.changeRole(localStorage.getItem("role")));
+    }
+  }, []);
   return (
     <div>
 
@@ -35,6 +54,11 @@ const App = () => {
         <Route path="/signup" element={<Signup/>}/>
         <Route path="/login" element={<Login/>}/>
         <Route path='/disease' element={<Disease/>}/>
+        <Route path="/reports" element={<Reports/>}/>
+        <Route path="/my-cattle" element={<MyCattle/>}/>
+        <Route path="/activity" element={<Activity/>}/> 
+        <Route path="/admin-marketplace" element={<AdminMarketplace/>}/>
+        <Route path="/help" element={<Help/>}/>
       </Routes>
       <VoicebotLogo/>
       <Footer />
