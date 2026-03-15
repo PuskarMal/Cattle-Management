6adc115f1672b1b5e4b179879f4d31e5fafe44c5
PuskarMal <puskarmal07@gmail.com>
2026-03-15 10:15:58 +0530

    port added

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import tensorflow as tf
 import numpy as np
+import os
 from PIL import Image
 from io import BytesIO
 from tensorflow.keras.preprocessing import image
@@ -94,4 +95,6 @@ def predict_disease():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000)) 
+    app.run(host="0.0.0.0", port=port,debug=True)
+    
