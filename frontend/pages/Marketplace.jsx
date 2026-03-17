100128f299042fbf74d9712dcfda05dc19931e5f
PuskarMal <puskarmal07@gmail.com>
2026-03-17 21:28:01 +0530

    categories added

--- frontend/pages/Marketplace.jsx
+++ frontend/pages/Marketplace.jsx
@@ -80,7 +80,7 @@ const SAMRIDHI_MARKETPLACE = () => {
               className="bg-white rounded-xl shadow-sm border p-4 flex flex-col"
             >
               <img
-                src={`https://cattle-management-ptz0.onrender.com/${product.image}`}
+                src={`https://cattle-management-ptz0.onrender.com${product.image}`}
                 alt={product.name || product.breed_name}
                 className="h-40 object-cover rounded-md mb-3"
               />
