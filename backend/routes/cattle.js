d467c05e23167e7841010b0391fb57e0d3e8d785
PuskarMal <puskarmal07@gmail.com>
2026-04-26 23:23:54 +0530

    district-based analysis integrated for admin - backend

--- backend/routes/cattle.js
+++ backend/routes/cattle.js
@@ -1,6 +1,7 @@
 const Cattle = require("../models/cattle");
 const router = require("express").Router();
 const User = require("../models/user");
+const Dist = require("../models/district");
 
 router.get("/my-cattle", async (req, res) => {
   
@@ -11,6 +12,25 @@ router.get("/my-cattle", async (req, res) => {
     });
   
   res.json(cattle);
+});
+
+router.get("/all-districts", async (req, res) => {
+  const districts = await Dist.find();
+  res.json(districts);
+});
+
+router.get("/district-analysis/:district", async (req, res) => {
+  const district = req.params.district;
+  const type = req.headers.type
+  if(type!="Total"){
+    const districtData = await Dist.findOne({ districtName:district, Type:type });  
+    res.json(districtData);
+  }
+  else{
+    const districtData = await Dist.find({districtName: district}, '-_id -__v');
+    res.json(districtData);
+  }
+  
 });
 
 module.exports = router;
